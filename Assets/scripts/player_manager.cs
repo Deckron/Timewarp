8a0b90c9eb12e515967be46f7dac45623c2c3866
Taino Rex <dvkenobii@gmail.com>
2019-10-17 16:29:30 -0400

    save system and animations part 1/incomplete

--- Assets/scripts/player_manager.cs
+++ Assets/scripts/player_manager.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class player_manager : MonoBehaviour
 {
+    public int level = 3;
+    public int health = 10;
+
+    public void SavePlayer()
+    {
+        save_system.savePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        player_data data = save_system.LoadPlayer();
+
+        level = data.level;
+        health = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+    }
     #region Singleton
     public static player_manager instance;
 
